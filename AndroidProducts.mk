45785e21e27273647a7bd0cbc2eb68a06c0fead4
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-22 17:17:47 +0200

    rhodep: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: motorola Model: moto g82 5G Script version: 2.4.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_rhodep.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_rhodep-user \
+    omni_rhodep-userdebug \
+    omni_rhodep-eng
